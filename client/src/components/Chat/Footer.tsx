55f20f6c6d67d446d0a942d91c4339a110d9a468
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2025-09-15 02:33:04 +0300

    personalized

--- client/src/components/Chat/Footer.tsx
+++ client/src/components/Chat/Footer.tsx
@@ -37,9 +37,7 @@ export default function Footer({ className }: { className?: string }) {
   const mainContentParts = (
     typeof config?.customFooter === 'string'
       ? config.customFooter
-      : '[LibreChat ' +
-        Constants.VERSION +
-        '](https://librechat.ai) - ' +
+      : '[AIN - OSOS](https://osos.om) - ' +
         localize('com_ui_latest_footer')
   ).split('|');
 
